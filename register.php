ddd06b0063386991a770808b1d43625c570675d0
James Acquoi Suworgo <39415473+asuworgo@users.noreply.github.com>
2018-09-21 10:02:07 -0700

    Add files via upload

---
+++ register.php
@@ -0,0 +1,69 @@
+<?php include('server.php'); ?>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <link type="text/css" rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <title>Registration</title>
+</head>
+<style>
+    .login-panel {
+        margin-top: 150px;
+    }
+    .error {
+        width: 92%;
+        margin: 0px auto;
+        padding: 10px;
+        border: 1px solid #a94442;
+        color: #a94442;
+        background: #f2dede;
+        border-radius: 5px;
+        text-align: left;
+    }
+    
+    .body{
+        background-image: url(https://laubplusco.net/wp-content/uploads/2015/01/pentia_sitecore_login.png);
+    }
+
+</style>
+<body>
+
+<div class="container"><!-- container class is used to centered  the body of the browser with some decent width-->
+    <div class="row"><!-- row class is used for grid system in Bootstrap-->
+        <div class="col-md-4 col-md-offset-4"><!--col-md-4 is used to create the no of colums in the grid also use for medimum and large devices-->
+            <div class="login-panel panel panel-success">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Registration</h3>
+                </div>
+                <div class="panel-body">
+                    <form role="form" method="post" action="registration.php">
+                        
+                        <fieldset>
+                            <div class="form-group">
+                                <input class="form-control" placeholder="Username" name="username" type="text" autofocus>
+                            </div>
+
+                            <div class="form-group">
+                                <input class="form-control" placeholder="E-mail" name="email" type="email" autofocus>
+                            </div>
+                            <div class="form-group">
+                                <input class="form-control" placeholder="Password" name="password_1" type="password" value="">
+                            </div>
+                            <div class="form-group">
+                                <input class="form-control" placeholder="Confirm password" name="password_2" type="password" value="">
+                            </div>
+
+
+                            <input class="btn btn-lg btn-success btn-block" type="submit" value="register" name="register" >
+
+                        </fieldset>
+                    </form>
+                    <center><b>Already registered ?</b> <br></b><a href="login.php">Login here</a></center><!--for centered text-->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+
+</html>
